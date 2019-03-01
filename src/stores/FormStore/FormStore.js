3cbda97479fa3fae6843bf396e2bc3e328a39307
Administrator <lumengbit2@gmail.com>
2019-03-02 02:18:56 +1100

    add locastorage

--- src/stores/FormStore/FormStore.js
+++ src/stores/FormStore/FormStore.js
@@ -1,21 +1,52 @@
 import {observable, action, configure, computed} from 'mobx';
+import {reactLocalStorage} from 'reactjs-localstorage';
 
 configure({ enforceActions: 'always' })
-
 class FormStore {
+
     @observable 
     formvalue = {
-        
+        name:'',
+        type:'',
+        shift:'',
+        starttime:'',
+        endtime:''
     }
 
     @action 
-    handleSubmit(form) {
-        console.log(form)
+    handleConfirm() {
+        console.log(this.formvalue.name);
+        reactLocalStorage.setObject('FormValue', this.formvalue);
+    }
+    @action 
+    formName(name){
+        this.formvalue.name=name;
+    }
+    @action 
+    formShift(shift){
+        this.formvalue.shift=shift;
+    }
+    @action 
+    formType(type){
+        this.formvalue.type=type;
+    }
+    @action 
+    formStarttime(starttime){
+        this.formvalue.starttime=starttime;
+    }
+    @action 
+    formEndtime(endtime){
+        this.formvalue.endtime=endtime;
+    }
+    @action 
+    handleSubmit() {
+      
+        console.log(reactLocalStorage.getObject('FormValue'));
     }
 
-    @computed get unfinishedTodos  () {
-      return  this.todos.filter((todo) => todo.done)
-    }
+    // @computed get unfinishedTodos  () {
+    //   return  this.todos.filter((todo) => todo.done)
+    // }
   }
 
-  export default FormStore;
+export default FormStore;
